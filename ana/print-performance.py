77870d3f9108bf2e8c7a65b4a0a65600f088dcb2
tomeichlersmith <eichl008@umn.edu>
2023-12-04 10:25:37 -0600

    add script to print performance data from test run

---
+++ ana/print-performance.py
@@ -0,0 +1,39 @@
+"""load a file and print the performance data"""
+
+import uproot
+import sys
+import json
+
+class ReprEncoder(json.JSONEncoder):
+    """if JSON can't serialize it, just use its repr string"""
+    def default(self, obj):
+        try:
+            return super().default(obj)
+        except TypeError:
+            return repr(obj)
+
+
+with uproot.open(sys.argv[1]) as f:
+    print('absolute')
+    t = f['performance/absolute'].members
+    print(t['duration_'], t['start_time_'])
+    for callback in [
+        'onProcessStart', 'onProcessEnd',
+        'onFileOpen','onFileClose',
+        'beforeNewRun', 'onNewRun'
+    ]:
+        print(callback)
+        for name, timer in f[f'performance/{callback}'].items():
+            print('  ', f'{name:10s}', f'{timer.members["duration_"]:.3e}', timer.members['start_time_'])
+
+    events = {}
+    t = f['performance/by_event']
+    for name in t.keys(recursive=False):
+        if len(t[name].values()) == 0:
+            events[name] = t[name].array(library='np')
+        else:
+            events[name] = {
+                subbr.name : subbr.array(library='np')
+                for subbr in t[name].values()
+            }
+    print(json.dumps(events, cls=ReprEncoder, indent=2))
--- config/log-performance.py
+++ config/log-performance.py
@@ -2,8 +2,13 @@ from LDMX.Framework import ldmxcfg
 p = ldmxcfg.Process('prod')
 p.run = 1
 p.maxEvents = 3
+p.maxTriesPerEvent = 10
+import sys
+if len(sys.argv) > 1:
+    p.maxTriesPerEvent = int(sys.argv[1])
 p.sequence = [
-    ldmxcfg.Producer('Produce','bench::Produce','Bench'),
+    ldmxcfg.Producer('MultiTry','bench::MultiTry','Bench'),
+    ldmxcfg.Producer('Recon', 'bench::Recon', 'Bench')
     ]
 p.termLogLevel = 0
 p.logFrequency = 1
